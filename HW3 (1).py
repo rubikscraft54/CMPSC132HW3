abea6526fd1162a3536c8cd6e5fce4f7032dd03d
ktpighini <40744947+ktpighini@users.noreply.github.com>
2019-11-03 22:58:42 -0500

    Add files via upload
    update

--- HW3 (1).py
+++ HW3 (1).py
@@ -6,205 +6,239 @@ Team members:
 Collaboration Statement:             
 
 '''
+import re
 
 class Node:
-    def __init__(self, value):
-        self.value = value  
-        self.next = None 
-    
-    def __str__(self):
-        return "Node({})".format(self.value) 
+	def __init__(self, value):
+		self.value = value  
+		self.next = None 
+	
+	def __str__(self):
+		return "Node({})".format(self.value) 
 
-    __repr__ = __str__
-                          
+	__repr__ = __str__
+						  
 
 #=============================================== Part I ==============================================
 
 class Stack:
-   
-    def __init__(self):
-        self.top = None
-        self.count=0
-    
-    def __str__(self):
-        temp=self.top
-        out=[]
-        while temp:
-            out.append(str(temp.value))
-            temp=temp.next
-        out='\n'.join(out)
-        return ('Top:{}\nStack:\n{}'.format(self.top,out))
+	'''
+		>>> x=Stack()
+		>>> x.pop()
+		>>> x.push(2)
+		>>> x.push(4)
+		>>> x.push(6)
+		>>> x
+		Top:Node(6)
+		Stack:
+		6
+		4
+		2
+		>>> x.pop()
+		6
+		>>> x
+		Top:Node(4)
+		Stack:
+		4
+		2
+		>>> len(x)
+		2
+		>>> x.peek()
+		4
+	'''
+	def __init__(self):
+		self.top = None
+		self.count=0
+	
+	def __str__(self):
+		temp=self.top
+		out=[]
+		while temp:
+			out.append(str(temp.value))
+			temp=temp.next
+		out='\n'.join(out)
+		return ('Top:{}\nStack:\n{}'.format(self.top,out))
 
-    __repr__=__str__
+	__repr__=__str__
 
-    def isEmpty(self):
-        if self.top is None:
-            return True
-        else:
-            return False
-        
-    def __len__(self): 
-        count = 0 
-        temp = self.top
-        while temp:
-            count+= 1
-            temp = temp.next
-        return count
+	def isEmpty(self):
+		if self.count == 0:
+			return True
+		else:
+			return False
 
-    def push(self,value):
-        if self.top is None:
-            self.top = Node(value)
-        else:
-            new_node = Node(value)
-            new_node.next = self.top
-            self.top = new_node
+	def __len__(self): 
+		return self.count
 
-    def pop(self):
-        if self.top is None:
-            print("stack is empty")
-            return None
-        else:
-            popped= self.top.value
-            self.top = self.top.next
-            return popped 
+	def push(self,value):
+		new = Node(value)
+		new.next = self.top
+		self.top = new
+		self.count += 1
+	 
+	def pop(self):
+		if self.isEmpty():
+			return None
+		else:
+			top = self.top
+			newTop = top.next
+			self.top = newTop
+			top.next = None
+			self.count -= 1
+			return top.value
 
-    def peek(self):
-        return self.top.value
-
-
+	def peek(self):
+		return self.top.value
 
 #=============================================== Part II ==============================================
 
 class Calculator:
-    def __init__(self):
-        self.expr = None
+	def __init__(self):
+		self.expr = None
 
 
-    def isNumber(self, txt):
- 		try:
-            float(txt)
-            return True
-        except ValueError:
-            return False
-    	
+	def isNumber(self, txt):
+		try:
+			txt = float(txt)
+			return True
+		except ValueError:
+			return False
 
 
-    def postfix(self, txt):
+	def postfix(self, txt):
+		'''
+			Required: postfix must create and use a Stack for expression processing
+			>>> x=Calculator()
+			>>> x.postfix(' 2 ^        4')
+			'2.0 4.0 ^'
+			>>> x.postfix('2')
+			'2.0'
+			>>> x.postfix('2.1*5+3^2+1+4.45')
+			'2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.45 +'
+			>>> x.postfix('    2 *       5.34        +       3      ^ 2    + 1+4   ')
+			'2.0 5.34 * 3.0 2.0 ^ + 1.0 + 4.0 +'
+			>>> x.postfix(' 2.1 *      5   +   3    ^ 2+ 1  +     4')
+			'2.1 5.0 * 3.0 2.0 ^ + 1.0 + 4.0 +'
+			>>> x.postfix('(2.5)')
+			'2.5'
+			>>> x.postfix ('((2))')
+			'2.0'
+			>>> x.postfix ('     -2 *  ((  5   +   3)    ^ 2+(1  +4))    ')
+			'-2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+			>>> x.postfix ('  (   2 *  ((  5   +   3)    ^ 2+(1  +4)))    ')
+			'2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+			>>> x.postfix ('  ((   2 *  ((  5   +   3)    ^ 2+(1  +4))))    ')
+			'2.0 5.0 3.0 + 2.0 ^ 1.0 4.0 + + *'
+			>>> x.postfix('   2 *  (  5   +   3)    ^ 2+(1  +4)    ')
+			'2.0 5.0 3.0 + 2.0 ^ * 1.0 4.0 + +'
+			>>> x.postfix('2 *    5   +   3    ^ -2       +1  +4')
+			'error message'
+			>>> x.postfix('2    5')
+			'error message'
+			>>> x.postfix('25 +')
+			'error message'
+			>>> x.postfix('   2 *  (  5   +   3)    ^ 2+(1  +4    ')
+			'error message'
+			>>> x.postfix('2*(5 +3)^ 2+)1  +4(    ')
+			'error message'
+		'''
+		if not isinstance(txt,str) or len(txt)<=0:
+			print("Argument error in postfix")
+			return None
 
-        if not isinstance(txt,str) or len(txt)<=0:
-            print("Argument error in postfix")
-            return None
-        postStack = Stack()
-        precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
-        output= []
-        txt = txt.split()
-        for i in txt:
-            if i in "0123456789":
-                output.append(i)
-            elif i == '(':
-                postStack.push(i)
-            elif i == ')':
-                head = postStack.pop()
-                while head != '(':
-                    output.append(head)
-                    head = postStack.pop()
-            else:
-                while not postStack.isEmpty() and precedence[postStack.peek()]<= precedence[i]:
-                    output.append(postStack.pop())
-                postStack.push(i)
-        while not postStack.isEmpty():
-            output.append(postStack.pop())
-        return " ".join(output)
-        # YOUR CODE STARTS HERE
-        
+		postStack=Stack()
+		output = []
+		precedence = {'+':1, '-':1, '*':2, '/':2, '^':3}
+		splitTxt = re.split('([-+*/()^])', txt)
+		infix = []
+		for x in splitTxt:
+			x = x.strip()
+			if x in '-+*/()^':
+				infix.append(x)
+			else:
+				try:
+					y = float(x)
+					infix.append(x)
+				except ValueError:
+					continue
+		infix = ' '.join(infix).split()
+		for i in infix:
+			if self.isNumber(i):
+				i = float(i)
+				if isinstance(i, int):
+					output.append(str(i)+'.0')
+				else:
+					output.append(str(float(i)))
+			elif i == '(':
+				postStack.push(i)
+			elif i == ')':
+				top = postStack.pop()
+				while (top != '(') and (not postStack.isEmpty()):
+					output.append(top)
+					top = postStack.pop()
+			elif i == ' ':
+				pass
+			else:
+				try:
+					while (not postStack.isEmpty()) and (precedence[i] <= precedence[postStack.peek()]):
+						output.append(postStack.pop())
+				except KeyError:
+					pass
+				finally:
+					postStack.push(i)
+		while not postStack.isEmpty():
+			output.append(postStack.pop())
+		return " ".join(output)
 
 
-    @property
-    def calculate(self):
-        '''
-            Required: calculate must call postfix
-                      calculate must create and use a Stack to compute the final result as shown in the video lecture
-            >>> x=Calculator()
-            >>> x.expr='    4  +      3 -2'
-            >>> x.calculate
-            5.0
-            >>> x.expr='  -2  +3.5'
-            >>> x.calculate
-            1.5
-            >>> x.expr='4+3.65-2 /2'
-            >>> x.calculate
-            6.65
-            >>> x.expr=' 23 / 12 - 223 +      5.25 * 4    *      3423'
-            >>> x.calculate
-            71661.91666666667
-            >>> x.expr='   2   - 3         *4'
-            >>> x.calculate
-            -10.0
-            >>> x.expr=' 3 *   (        ( (10 - 2*3)))'
-            >>> x.calculate
-            12.0
-            >>> x.expr=' 8 / 4  * (3 - 2.45      * (  4- 2 ^   3)) + 3'
-            >>> x.calculate
-            28.6
-            >>> x.expr=' 2   *  ( 4 + 2 *   (5-3^2)+1)+4'
-            >>> x.calculate
-            -2.0
-            >>> x.expr='2.5 + 3 * ( 2 +(3.0) *(5^2 - 2*3^(2) ) *(4) ) * ( 2 /8 + 2*( 3 - 1/ 3) ) - 2/ 3^2'
-            >>> x.calculate
-            1442.7777777777778
-            >>> x.expr="4++ 3 +2"
-            >>> x.calculate
-            'error message'
-            >>> x.expr="4    3 +2"
-            >>> x.calculate
-            'error message'
-            >>> x.expr='(2)*10 - 3*(2 - 3*2)) '
-            >>> x.calculate
-            'error message'
-            >>> x.expr='(2)*10 - 3*/(2 - 3*2) '
-            >>> x.calculate
-            'error message'
-            >>> x.expr=')2(*10 - 3*(2 - 3*2) '
-            >>> x.calculate
-            'error message'
-        '''
+	@property
+	def calculate(self):
+		if not isinstance(self.expr,str) or len(self.expr)<=0:
+			print("Argument error in calculate")
+			return None
+		calcStack=Stack()
+		return self._calculate
 
-        if not isinstance(self.expr,str) or len(self.expr)<=0:
-            print("Argument error in calculate")
-            return None
+	@calculate.setter
+	def calculate(self, value):
 
-        calcStack=Stack()
-        postfix_expr = Calculator().postfix(self.expr)
-        for i in postfix_expr:
-        
-            if i.isdigit():
-                calcStack.push(i)
-            if i == '^':
-                num1= calcStack.pop()
-                num2= calcStack.pop()
-                result= num2 ** num1
-                calcStack.push(result)
-            elif i == '*':
-                num1= calcStack.pop()
-                num2= calcStack.pop()
-                result = num1 * num2
-                calcStack.push(result)
-            elif i == '/':
-                num1= calcStack.pop()
-                num2= calcStack.pop()
-                result = num2 / num1
-                calcStack.push(result)
-            elif i == '-':
-                num1= calcStack.pop()
-                num2= calcStack.pop()
-                result = num2 - num1
-                calcStack.push(result)
-            elif i == '+':
-                num1= calcStack.pop()
-                num2= calcStack.pop()
-                result= num2 + num1
-                calcStack.push(result)
-            return calcstack.pop()
-           
+		calcStack=Stack()
+		postfix_expr = Calculator().postfix(self.expr)
+		print(postfix_expr)
+		for i in postfix_expr:
+			if i in "0123456789":
+				calcStack.push(i)
+			elif i == '^':
+				num1= calcStack.pop()
+				num2= calcStack.pop()
+				result= float(num2) ** float(num1)
+				calcStack.push(result)
+			elif i == '*':
+				num1= calcStack.pop()
+				num2= calcStack.pop()
+				result = float(num1) * float(num2)
+				calcStack.push(result)
+			elif i == '/':
+				num1= calcStack.pop()
+				num2= calcStack.pop()
+				result = float(num2) / float(num1)
+				calcStack.push(result)
+			elif i == '-':
+				num1= calcStack.pop()
+				num2= calcStack.pop()
+				result = float(num2) - float(num1)
+				calcStack.push(result)
+			elif i == '+':
+				num1= calcStack.pop()
+				num2= calcStack.pop()
+				result= float(num2) + float(num1)
+				calcStack.push(result)
+		value =  calcStack.pop()
+		self.calculate = value
+		
+
+x=Calculator
+x.expr='    4  +      3 -2'
+x.calculate()
 
        
